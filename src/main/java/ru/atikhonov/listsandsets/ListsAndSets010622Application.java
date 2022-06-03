a5972a608408c07a293f0f9f0a7e6101ad68fae9
a2tikhonov <git!St@d1um@rc@d1um>
2022-06-03 12:18:39 +0300

    Initial Commit

---
+++ src/main/java/ru/atikhonov/listsandsets/ListsAndSets010622Application.java
@@ -0,0 +1,13 @@
+package ru.atikhonov.listsandsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsAndSets010622Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListsAndSets010622Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/atikhonov/listsandsets/ListsAndSets010622ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.atikhonov.listsandsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsAndSets010622ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
